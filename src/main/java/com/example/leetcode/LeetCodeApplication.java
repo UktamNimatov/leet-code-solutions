e9c90845acef860c1d67842f109131c3cb616ad4
Uktam Nimatov <uktamnimatov@Uktams-MacBook-Pro-2.local>
2024-06-11 14:18:24 +0500

    Provide solution to leetcode N48 problem

--- src/main/java/com/example/leetcode/LeetCodeApplication.java
+++ src/main/java/com/example/leetcode/LeetCodeApplication.java
@@ -8,6 +8,15 @@ public class LeetCodeApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(LeetCodeApplication.class, args);
+        System.out.println(reverseBits("00000010100101000001111010011100"));
     }
 
+    public static int reverseBits(String n) {
+        int result = 0;
+        for (int i = 0; i < 32; i++) {
+            result = (result << 1) + (n & 1);
+            n = n >> 1;
+        }
+        return result;
+    }
 }
---
+++ src/main/java/com/example/leetcode/problems/question39/Solution.java
@@ -0,0 +1,10 @@
+package com.example.leetcode.problems.question39;
+
+import java.util.List;
+
+public class Solution {
+    public List<List<Integer>> combinationSum(int[] candidates, int target) {
+
+        return null;
+    }
+}
